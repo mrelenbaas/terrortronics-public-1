459a4c84abe864243efbb0d7aa1cb315928bef1a
Martian__Knight <mr.elenbaas@gmail.com>
2023-11-07 13:32:27 -0600

    Removed usb_forms Arduino project.

--- MIT/Arduino Projects/usb_forms/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['messages_0',['messages',['../usb__forms_8ino.html#a7048605d09bb21159ccaab63402dc4e5',1,'usb_forms.ino']]]
-];
--- MIT/Arduino Projects/usb_forms/html/search/functions_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['setup_0',['setup',['../usb__forms_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'usb_forms.ino']]],
-  ['start_1',['start',['../usb__forms_8ino.html#a60de64d75454385b23995437f1d72669',1,'usb_forms.ino']]]
-];
