6cdaca94f92f4903ce611384fea89c531067aa36
Martian__Knight <mr.elenbaas@gmail.com>
2023-11-07 16:52:38 -0600

    Removed usb_pygame Arduino project.

--- MIT/Arduino Projects/usb_pygame/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['light_5fready_5foff_0',['LIGHT_READY_OFF',['../usb__pygame_8ino.html#a4ab31e920e566b844aa76b7a45bc676faf46aef8315090c09d85027eb8482a935',1,'usb_pygame.ino']]],
-  ['light_5fready_5fon_1',['LIGHT_READY_ON',['../usb__pygame_8ino.html#a4ab31e920e566b844aa76b7a45bc676facc96e6f56c6678f6eed27ea03a790116',1,'usb_pygame.ino']]],
-  ['loop_2',['loop',['../usb__pygame_8ino.html#afe461d27b9c48d5921c00d521181f12f',1,'usb_pygame.ino']]]
-];
--- MIT/Arduino Projects/usb_pygame/html/search/enumvalues_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['resetmessage_0',['resetMessage',['../usb__pygame_8ino.html#a7048605d09bb21159ccaab63402dc4e5a5de5667ec04fdfc88fc1666cc1a6e41f',1,'usb_pygame.ino']]]
-];
