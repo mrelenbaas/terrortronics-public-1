77fc9104ca8d6053b360ebc0f88498d633a9ec06
Martian__Knight <mr.elenbaas@gmail.com>
2023-11-29 20:42:16 -0600

    Updated arcade_controller Arduino project.

---
+++ MIT/Arduino Projects/arcade_controller/arcade_controller.h
@@ -0,0 +1,189 @@
+/**
+   @file arcade_controller.h
+
+   @mainpage Arcade Controller Project
+
+   @section description Description
+   The Arcade Controller.
+
+   @section circuit Circuit
+   - Buttons.
+   - Switches.
+
+   @section libraries Libraries
+   - Serial
+
+   @section notes Notes
+   - The Overview section of the Doxygen docs does not include print
+   statements. The inline comments are the same as the Overview
+   section, but they do include print statements.
+
+   @section author Author
+   - Bradley Elenbaas (mr.elenbaas@gmail.com)
+   - Version: 2
+   - Date: November 29, 2023
+
+   @section ip Intellectual Property
+   Copyright (c) 2023 Bradley Elenbaas. All rights
+   reserved.
+
+   @section license License
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation files
+   (the “Software”), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software,
+   and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+
+   @section description Description
+   Empty.
+
+   @section pins Pins
+   - empty
+
+   @section reference Resources
+   - Documentation
+    + Arduino Due Hardware:
+        - Incomplete pinout. https://docs.arduino.cc/hardware/due
+        - Complete pinout. https://forum.arduino.cc/t/due-pinout-diagram/129258
+    + Software:
+        - https://www.arduino.cc/reference/en/
+   - Debouncing
+    + Time Based:
+        - https://www.arduino.cc/en/Tutorial/BuiltInExamples/Debounce
+    + Current/Previous Based:
+        - https://docs.arduino.cc/built-in-examples/digital/Debounce
+
+   @section warnings WARNINGS
+   - empty
+
+   @section ut Unit Tests
+   - empty
+*/
+
+////////////////////////////////////////////////////////////////////////
+// Function Stubs //////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+// Messages.
+void resetFunction();
+void startFunction();
+
+////////////////////////////////////////////////////////////////////////
+// Serial //////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/**
+   The serial baud rate.
+*/
+const int BAUD_RATE = 9600;
+
+////////////////////////////////////////////////////////////////////////
+// Messages ////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+/**
+   An enum of possible message codes.
+*/
+enum messages {
+  startMessage = 48, ///< Start message.
+  resetMessage = 49  ///< Reset message.
+};
+/**
+   The default outgoing message.
+*/
+const char OUTGOING_START[] = {
+  't',
+  'y',
+  'p',
+  'e',
+  ':',
+  'c',
+  'o',
+  'n',
+  'f',
+  'i',
+  'g',
+  ',',
+  'f',
+  'i',
+  'l',
+  'e',
+  'n',
+  'a',
+  'm',
+  'e',
+  ':',
+  'f',
+  'p',
+  's',
+  ',',
+  'f',
+  'u',
+  'n',
+  'c',
+  't',
+  'i',
+  'o',
+  'n',
+  ':',
+  'r',
+  'e',
+  's',
+  'e',
+  't',
+  ',',
+  'd',
+  'e',
+  'l',
+  'i',
+  'm',
+  'i',
+  't',
+  'e',
+  'r',
+  's',
+  ':',
+  ' ',
+  'c',
+  'm',
+  '0',
+  ';',
+  ' ',
+  'c',
+  'm',
+  '1',
+  ';',
+  ' ',
+  'c',
+  'm',
+  '2',
+  ';',
+  ' ',
+  'c',
+  'm',
+  '3',
+  ';',
+  ' ',
+  'c',
+  'm',
+  '4',
+  ';',
+  '\\',
+  'n',
+  '\0'
+};
+/**
+   The incoming message.
+*/
+int incomingMessage;
